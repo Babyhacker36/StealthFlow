67d8c37d53503bcd27ffff920770cef45e5b1ba3
Carter Louis <Louiscarterjr29@gmail.com>
2026-02-09 02:43:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1488088854-518ba",
+  "appId": "1:773107038903:web:574aa038d9d86e5e01eb32",
+  "apiKey": "AIzaSyDoJm_dDJtaM3b173ysWz_lkOazUSdXcWk",
+  "authDomain": "studio-1488088854-518ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "773107038903"
+};
